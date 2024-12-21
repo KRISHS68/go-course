419c9dec0edb2679cd1c7e8057a304cf6dd8339f
Srinidhi Krishnamurthy - s0k067g <wmtnoreply-s0k067G@walmart.com>
2024-12-21 12:27:18 +0530

    views

--- lenslocked/go.mod
+++ lenslocked/go.mod
@@ -2,4 +2,7 @@ module github.com/krishs68/lenslocked
 
 go 1.21.2
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+)
--- lenslocked/main.go
+++ lenslocked/main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/krishs68/lenslocked/views"
 )
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
@@ -37,23 +40,38 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; cahrset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>`)
+	tplPath := filepath.Join("templates", "faq.go.html")
+	executeTemplate(w, tplPath)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html;charset=utf-8")
-	w.WriteHeader(200)
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	tplPath := filepath.Join("templates", "home.go.html")
+	executeTemplate(w, tplPath)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
+	tplPath := filepath.Join("templates", "contact.go.html")
+	executeTemplate(w, tplPath)
+}
+
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	t, err := views.Parse(filepath)
+	if err != nil {
+		log.Print("parsing template:%v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, nil)
 }
 
 func main() {
 
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
 
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
